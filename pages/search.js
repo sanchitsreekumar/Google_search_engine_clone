4a6ab2e01896135c748f55344e75315b694431c9
Sanchit <sanchit.sreekumar77@gmail.com>
2022-04-07 13:15:02 +0530

    Updated keys

--- pages/search.js
+++ pages/search.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
 
-import {API_KEY, CONTEXT_KEY} from '../keys'
+
 import Response from '../Response';
 import { useRouter } from 'next/router';
 import SearchResults from '../components/SearchResults';
@@ -34,7 +34,7 @@ export async function getServerSideProps(context){
     const useDummyData = false; // to prevent exhausting the api calls
     const startIndex = context.query.start || '0'
 
-    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`) 
+    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`) 
     .then(res => res.json());
 
     //after server has rendered pass the result to the client
